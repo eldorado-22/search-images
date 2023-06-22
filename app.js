bb694e0d428d01167271e929f4289ed6e50b7f8e
eldorado-22 <jumashevv992@gmail.com>
2023-06-22 15:35:03 +0600

    search-images

---
+++ app.js
@@ -0,0 +1,18 @@
+const searchForm = document.getElementById("search-form")
+const searchBox = document.getElementById("search-box")
+const searchResult = document.getElementById("search-result")
+const searchMoreBtn = document.getElementById("show-more-btn")
+
+
+
+let keyword = "";
+let page = 1;
+
+
+async function searchImages() {
+     keyword = searchBox.value;
+     const url = "https://api.unsplash.com/search/photos?page=1&query=office"
+}
+
+
+
